462da578f049a43903a83d8aede191166b5f7a04
kshateesh <kshateesh007@gmail.com>
2024-02-28 15:49:41 +0530

    map contentstack environment variables

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+//set VUE_APP_ prefix to Contentstack environment variables
+(function () {
+  process.env.VUE_APP_CONTENTSTACK_API_KEY = process.env.CONTENTSTACK_API_KEY;
+  process.env.VUE_APP_CONTENTSTACK_API_HOST = process.env.CONTENTSTACK_API_HOST;
+  process.env.VUE_APP_CONTENTSTACK_DELIVERY_TOKEN =
+    process.env.CONTENTSTACK_DELIVERY_TOKEN;
+  process.env.VUE_APP_CONTENTSTACK_ENVIRONMENT =
+    process.env.CONTENTSTACK_ENVIRONMENT;
+  process.env.VUE_APP_CONTENTSTACK_CDN = process.env.CONTENTSTACK_CDN;
+})();
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
